1434d3921266d73351ad309ef0d65338cc0291eb
parag dhundale <paragdhundale153@gmail.com>
2025-07-31 21:59:09 +0530

    Update Portfolio Project - Data Cleaning.sql

--- Portfolio Project - Data Cleaning.sql
+++ Portfolio Project - Data Cleaning.sql
@@ -26,7 +26,7 @@ SELECT * FROM world_layoffs.layoffs;
 
 -- 1. Remove Duplicates
 
-# First let's check for duplicates
+
 
 
 
@@ -59,9 +59,7 @@ SELECT *
 FROM world_layoffs.layoffs_staging
 WHERE company = 'Oda'
 ;
---  these are all legitimate entries and shouldn't be deleted. We need to really look at every single row to be accurate
 
--- these are our real duplicates 
 SELECT *
 FROM (
 	SELECT company, location, industry, total_laid_off,percentage_laid_off,`date`, stage, country, funds_raised_millions,
@@ -74,7 +72,7 @@ FROM (
 WHERE 
 	row_num > 1;
 
--- these are the ones we want to delete where the row number is > 1 or 2or greater essentially
+
 
 
 WITH DELETE_CTE AS 
@@ -217,7 +215,7 @@ WHERE industry IS NULL
 OR industry = ''
 ORDER BY industry;
 
--- ---------------------------------------------------
+
 
 
 SELECT DISTINCT industry
@@ -233,7 +231,7 @@ SELECT DISTINCT industry
 FROM world_layoffs.layoffs_staging2
 ORDER BY industry;
 
--- --------------------------------------------------
+
  
 
 SELECT *
